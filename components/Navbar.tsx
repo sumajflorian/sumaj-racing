6dac16070956c16e0f91bf665919409132c099c5
sumajflorian <y5frvtndsw@privaterelay.appleid.com>
2026-06-10 10:14:52 +0200

    Navbar2.0.tsx
    Logo Grösse geändert

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
         <img
           src="/logo.png"
           alt="Sumaj Racing Logo"
-          className="h-14 w-auto"
+          className="h-20 w-auto"
         />
 
         <div className="hidden gap-8 text-sm font-semibold uppercase tracking-wide md:flex">
